653a1cc702f66260921199acd7c44adba7a750bb
Magui Martin <Magali Martin>
2023-10-28 19:34:27 -0300

    deploy

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -9,38 +9,40 @@ export const FILTER_DOGS_BY_TEMPERAMENT = "FILTER_DOGS_BY_TEMPERAMENT"
 export const FILTER_BY_SITE = "FILTER_BY_SITE"
 export const SORT_BY_NAME = "SORT_BY_NAME"
 export const SORT_BY_WEIGHT = "SORT_BY_WEIGHT"
+const URL = process.env.REACT_APP_URL;
 
 export const getDogs = () => {
     return async function (dispatch) {
-        const dogs = (await axios.get("http://localhost:3001/dogs")).data
+        console.log(URL);
+        const dogs = (await axios.get(`${URL}/dogs`)).data
         return dispatch({type: GET_DOGS, payload: dogs})
     }
 }
 
 export const getById = (id) => {
     return async function (dispatch) {
-        const dogDetail = await axios.get(`http://localhost:3001/dogs/${id}`)
+        const dogDetail = await axios.get(`${URL}/dogs/${id}`)
         dispatch({type: GET_BY_ID, payload: dogDetail.data})
     }
 }
 
 export const getTemperaments = () => {
     return async function (dispatch){
-        const temperaments = await axios.get(`http://localhost:3001/temperaments`)
+        const temperaments = await axios.get(`${URL}/temperaments`)
         dispatch({type: GET_TEMPERAMENTS, payload: temperaments.data})
     }
 }
 
 export const createDog = (payload) => {
     return async function (dispatch){
-        await axios.post("http://localhost:3001/dogs", payload)
+        await axios.post(`${URL}/dogs`, payload)
         dispatch({type: CREATE_DOGS})
     }
 }
 
 export const filterByName = (name) => {
     return async function (dispatch) {
-        const dog = await axios.get(`http://localhost:3001/dogs/?name=${name}`)
+        const dog = await axios.get(`${URL}/dogs/?name=${name}`)
         dispatch({type: FILTER_BY_NAME, payload: dog.data})
     }
 }
